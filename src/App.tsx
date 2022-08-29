e3fd5a52700739ebbaa26df058d1b24547106d04
Renko <ren.tran@protonmail.com>
2022-08-29 01:41:36 -0700

    working, no animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import ImageCarousel from "./components/ImageCarousel";
 
 function App() {
+  const NUM_IMAGES = 8;
+  const [images, setImages] = useState([]);
+
+  async function fetchImages(num: number) {
+    await fetch(
+      `https://api.thecatapi.com/v1/images/search?limit=${num}&api_key= live_ioaRkIuTfIezHHWkZ6rJsDgLfDiXPG4jk8LrnjfMogBqNp1i9cYOqU1OLFASHIm4`
+    )
+      .then((res) => res.json())
+      .then((data) => setImages(data.map((img: { url: string }) => img.url)));
+  }
+
+  useEffect(() => {
+    fetchImages(NUM_IMAGES);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ImageCarousel images={images} />
     </div>
   );
 }
---
+++ src/components/ImageCarousel.tsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react";
+
+interface ImageCarouselProps {
+  images: Array<string>;
+}
+
+// no need to reset timer for some reason.
+
+export default function ImageCarousel({ images = [] }: ImageCarouselProps) {
+  const INTERVAL = 3000;
+  let TIMER: ReturnType<typeof setInterval>;
+  const SLOTS = images.length;
+  const [imageIndex, setImageIndex] = useState<number>(0);
+
+  function changeIndex(index: number) {
+    if (index < 0) {
+      setImageIndex(SLOTS - 1);
+    } else if (index >= SLOTS) {
+      setImageIndex(0);
+    } else {
+      setImageIndex(index);
+    }
+  }
+
+  // start carousel timer
+  useEffect(() => {
+    if (SLOTS === 0) return;
+    TIMER = setInterval(() => {
+      changeIndex(imageIndex + 1);
+    }, INTERVAL);
+    console.log("tick");
+    return () => clearInterval(TIMER); // clear on dismount
+  });
+
+  // log current index
+  useEffect(() => {
+    console.log(`Current index: ${imageIndex}`);
+  }, [imageIndex]);
+
+  return (
+    <div className="carousel">
+      <div className="left-arrow-container">
+        <div className="left-arrow" onClick={() => changeIndex(imageIndex - 1)}>
+          <i className="gg-chevron-left" />
+        </div>
+      </div>
+      <div className="right-arrow-container">
+        <div
+          className="right-arrow"
+          onClick={() => changeIndex(imageIndex + 1)}
+        >
+          <i className="gg-chevron-right" />
+        </div>
+      </div>
+      <div className="slot-container">
+        {images.map((img, index) => (
+          <span
+            className={`slot ${index === imageIndex ? "lit" : "none"}`}
+            key={index}
+            onClick={() => changeIndex(index)}
+          />
+        ))}
+      </div>
+      <div className="image-container">
+        {images.map((img, index) => (
+          <img
+            className="carousel-image"
+            src={img}
+            key={index}
+            alt={img}
+            style={{ display: `${index === imageIndex ? "block" : "none"}` }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
